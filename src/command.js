501abb7fb2201f83816902fb52094a6af47e6688
Baptiste <baptiste@Baptistes-Mac.local>
2019-01-01 14:06:36 +0100

    supprimé XRegeXp

--- src/command.js
+++ src/command.js
@@ -1,40 +1,27 @@
 const sketch = require("sketch");
 const Settings = require("sketch/settings");
-const Diff = require("diff");
 const searchAllTextLayers = require("./utils.js");
-const Xregexp = require('xregexp');
-var merge = require('lodash.merge');
-merge(Xregexp.XRegExp, require('xregexp-lookbehind'));
 const document = sketch.getSelectedDocument();
 
 //const toArray = require('sketch-utils/to-array');
 //const utils = require('sketch-utils');
 
-// CHARECTHER CONSTANTS
-const ELLIPSIS = Xregexp("\\u{2026}", 'gxu');
-const SPACE = Xregexp("\\u{0020}", 'gxu');		// Good ol' space
-const WNBSP = Xregexp("\\u{00A0}", 'gxu');   // wide non breakable space
-const NNBSP = Xregexp("\\u{202F}", 'gxu');   // narrow non breakable space
+// CHARACTER CONSTANTS
+const ELLIPSIS = "\u2026";
+const SPACE = "\u0020";		// Good ol' space
+const WNBSP = "\u00A0";   // wide non breakable space
+const NNBSP = "\u202F";   // narrow non breakable space
 const OPENING_QUOTE = "«";
 const CLOSING_QUOTE = "»";
 
 // REGEXs
-const NBSP_DOUBLE_PUNCTUATION = Xregexp(`(\\w+(?:\\s?»)?)(\\s?)([?!;:])(\\s|$)` , 'gxu');
-const REGEX_ELLIPSIS = Xregexp('(\\.{2,5})|(\\. \\. \\.)', 'gxu');
-
-// const DOUBLE_QUOTE_OPEN     = Xregexp(
-// 	`( "(?=\\w) ) 						# guillemet suivi d'un mot
-// 	|													# ou bien
-// 	 ( (?<=\\s|^)"(?=\\S) ) # espace ou début de texte, guillemet, tout sauf espace
-// 	 `
-// , 'x');
-// const DOUBLE_QUOTE_CLOSE    = Xregexp('(?: (?<=\\w)" ) | (?: (?<=\\S)"(?=\\s|$ )', 'x');
-
-const DOUBLE_QUOTE_OPEN = Xregexp('"(\\S)', 'xgu');
-const DOUBLE_QUOTE_CLOSE = Xregexp('(\\S)"', 'xgu')
-const NBSP_AFTER_QUOTE = Xregexp("(\\s|^|\\'|\\‘|\\’)(«)(\\s?)(\\w+)", 'xgu') 
-const NBSP_BEFORE_QUOTE = Xregexp('(\\w+[.?!]?)(\\s?)(»)(\\s|[.,?!:]|$)', 'xgu') 
-const ANY_NUMBER_EXCEPT_ONE = "(?!1\\b)d+"; 
+const NBSP_DOUBLE_PUNCTUATION = /(\w+(?:\s?»)?)(\s?)([?!;:])(\s|$)/gu;
+const REGEX_ELLIPSIS = /(\.{2,5})|(\. \. \.)/gu;
+const DOUBLE_QUOTE_OPEN = /"(\S)/gu;
+const DOUBLE_QUOTE_CLOSE = /(\S)"/gu;
+const NBSP_AFTER_QUOTE = /(\s|^|\'|\‘|\’)(«)(\s?)(\w+)/gu; 
+const NBSP_BEFORE_QUOTE = /(\w+[.?!]?)(\s?)(»)(\s|[.,?!:]|$)/gu ;
+const ANY_NUMBER_EXCEPT_ONE = /(?!1\b)d+/gu; 
 
 // SETTINGS
 let DEBUG = true;
@@ -79,33 +66,6 @@ export function replaceWNBSPbyNNBSP(context) {
   });
 }
 
-// function spaceInUnicode(str) {
-// 	let newstring = str.replace(/({u}00A0|{u}202F)/, function (match, p1) {
-// 		return `${p1.charCodeAt().toString(16)}`;
-// 	})
-// 	return newstring
-// }
-
-//fonction qui texte les regex : comparaison entre chaines après remplacement et chaines de référence
-export function testRegex() {
-  const referenceString =
-	"L’Histoire ne fait rien, elle ne « possède » pas de « richesse immense », elle « ne livre point de combats » ! C’est plutôt l’homme, l’homme réel et vivant, qui fait tout cela, qui possède et combat. Ce n’est certes pas l’« Histoire » qui se sert de l’homme comme moyen pour œuvrer et parvenir – comme si elle était un personnage à part – à ses propres fins ; au contraire, elle n'est rien d’autre que l’activité de l'homme – et rien que de l'homme – poursuivant ses fins… Y a-t-il une suite à ce texte ?\n";
-
-  const toFixString =
-	"L’Histoire ne fait rien, elle ne « possède» pas de «richesse immense », elle « ne livre point de combats » ! C’est plutôt l’homme, l’homme réel et vivant, qui fait tout cela, qui possède et combat. Ce n’est certes pas l’« Histoire » qui se sert de l’homme comme moyen pour œuvrer et parvenir – comme si elle était un personnage à part – à ses propres fins ; au contraire, elle n'est rien d’autre que l’activité de l'homme - et rien que de l'homme -- poursuivant ses fins… Y a-t-il une suite à ce texte?\n";
-
-  //const referenceString = "Y a-t-il une suite à ce texte ?";
-  //const toFixString ="Y a-t-il une suite à ce texte ?";
-
-	const fixedString = replaceString(toFixString).string;
-  if (fixedString == referenceString) {
-		console.log('test : succès');
-  } else {
-		
-		console.log("référence \t", spaceInUnicode(referenceString), "résultat \t", spaceInUnicode(fixedString), "origine \t",spaceInUnicode(toFixString));
-		console.log("\n\n test : erreur \n",  Diff.diffChars(fixedString, referenceString));
-  }
-}
 
 export function createCheckbox(setting, frame) {
 	let checkbox = NSButton.alloc().initWithFrame(frame);
@@ -170,131 +130,130 @@ export function openSettings(context) {
 export function replaceString(string) {
 
 let count = 0;
-string = Xregexp.replaceEach(string, [
+string = string.replace(
 
 									 // REMPLACEMENTS
 
 				 
 	// points de suspension
 
-		[REGEX_ELLIPSIS, function(match) {
+		REGEX_ELLIPSIS, function(match) {
 			console.log("points de suspension");
 			count++;
 			return ELLIPSIS;		
-		},  'all'],	
+		}).	
 
 		//incises intelligentes
-		["([^0-9]\\s)--?(\\s?[^0-9])", function ( match, $1, $2, $3) {
+		replace(/([^0-9]\s)--?(\s?[^0-9])/, function ( match, $1, $2, $3) {
 			console.log("incises intelligentes");
 			count++;
-			return `${p1}–${p2}`;
-		},  'all'],
+			return `${$1}–${$1}`;
+		}).
 
 		// puces en début de ligne
-		["(^|\\n|\\r)--?", function (match, $1) {
+		replace(/(^|\n|\r)--?/, function (match, $1) {
 			console.log("puces en début de ligne");
 			count++;
 			return "–";
-		},  'all'],
+		}).
 
 		//  n° --> №
-		["n°", function (match, $1, $2, $3) {
+		replace(/n°/, function (match, $1, $2, $3) {
 			count++;
 			console.log("n°");
 			return "№";
-		},  'all'],
+		}).
 
 		// 1/2, 1/3, 1/4 --> caractères dédiés pour ces fractions
-		["(\\s|\\w|^)1\\/2(\\s|\\w|$)", function ( match, $1, $2) {
+		replace(/(\s|\w|^)1\/2(\s|\w|$)/, function ( match, $1, $2) {
 			count++;
 			console.log("1/2");
 			return `${$1}½${$2}`;
-		},  'all'],
+		}).
 
-		["(\\s|\\w|^)1\\/3(\\s|\\w|$)", function ( match, $1, $2) {
+		replace(/(\s|\w|^)1\/3(\s|\w|$)/, function ( match, $1, $2) {
 			count++;
 			console.log("1/3");
 			return `${$1}⅓${$2}`;
-		},  'all'],
+		}).
 
-		["(\\s|\\w|^)1\\/4(\\s|\\w|$)", function ( match, $1, $2) {
+		replace(/(\s|\w|^)1\/4(\s|\w|$)/, function ( match, $1, $2) {
 			count++;
 			console.log("1/4");
 			return `${$1}¼${$2}`;
-		},  'all'],
+		}).
 
 		// 1er --> ordinal en exposant
-		["\\b1er?\\b", function ( match, $1, $2) {
+		replace(/\b1er?\b/, function ( match, $1, $2) {
 			count++;
 			console.log("1er --> ordinal en exposant");
 			return `1ᵉʳ`;
-		},  'all'],
+		}).
 
 		//2e --> ordinal en exposant
-		["(?!1\\b)(\\d+)e\\b", function ( match, $1, $2) {
+		replace(/(?!1\b)(\d+)e\b/, function ( match, $1, $2) {
 			count++;
 			console.log("2e --> ordinal en exposant");
 			return `${$1}ᵉ`;
-		},  'all'],
+		}).
 
 
 //  ESPACES INSÉCABLES
-
 	
 
 		// remplace " par «
-		[DOUBLE_QUOTE_OPEN, function( match, $1) {
+		replace(DOUBLE_QUOTE_OPEN, function( match, $1) {
 			count++
 			return OPENING_QUOTE + $1;
-		}, 'all'],
+		}).
 
 		//remplace " par »
-		[DOUBLE_QUOTE_CLOSE, function( match, $1) {
+		replace(DOUBLE_QUOTE_CLOSE, function( match, $1) {
 			count++
 			return $1 + CLOSING_QUOTE;
-		}, 'all'],
+		}).
 
 		//ajoute espace après «
-		[NBSP_AFTER_QUOTE, function ( match, $1, $2, $3, $4) {
+		replace(NBSP_AFTER_QUOTE, function ( match, $1, $2, $3, $4) {
 			console.log("//après «");
 			count++;
 			return  $1 + $2 + NBSP + $4;;
-		},  'all'],
+		}).
 
 		//espaces fines insécables avant ? ! ; :
-		[NBSP_DOUBLE_PUNCTUATION, function ( match, $1, $2, $3, $4) {
+		replace(NBSP_DOUBLE_PUNCTUATION, function ( match, $1, $2, $3, $4) {
 			console.log("espaces fines insécables avant ? ! ; :");
 			count++;
 			return `${$1}${NBSP}${$3}${$4}`;
-		},  'all'],
+		}).
 
 		//ajoute espace avant »
-		[NBSP_BEFORE_QUOTE, function ( match, $1, $2, $3, $4) {
+		replace(NBSP_BEFORE_QUOTE, function ( match, $1, $2, $3, $4) {
 			console.log("//avant »");
 			count++;
 			return $1 + NBSP + $3 + $4;
-		},  'all'],
+		}).
 
 		//avant %
-		["(\\d+)\\s?\\%", function ( match, $1, $2) {
+		replace(/(\d+)\s?\%/, function ( match, $1, $2) {
 			console.log("//avant %");
 			count++;
 			return `${$1}${NBSP}%`;
-		},  'all'],
+		}).
 
 		//avant $£€
-		["(\\d+)\\s?([$£€])", function ( match, $1, $2, $3) {
+		replace(/(\d+)\s?([$£€])/, function ( match, $1, $2, $3) {
 			console.log("/avant $£€");
 			count++;
 			return `${$1}${NBSP}${$2}`;
-		},  'all']
+		});
 		// /(\d{3})( |\D|$)", function (match, p1, p2) {
 		//     console.log('milliers')
 		//     count++;
 		//     return `${p1}${NNBSP}`;
 		// })
 
-]);
+
 
   return {
 	string: string,
